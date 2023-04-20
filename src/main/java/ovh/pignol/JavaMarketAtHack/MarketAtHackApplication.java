2c77301bacd55efdf9153bde17082a7e36242473
Patrick PIGNOL <patrick.pignol@gmail.com>
2023-04-20 03:12:17 +0200

    Update MarketAtHackApplication.java

--- src/main/java/ovh/pignol/JavaMarketAtHack/MarketAtHackApplication.java
+++ src/main/java/ovh/pignol/JavaMarketAtHack/MarketAtHackApplication.java
@@ -3,15 +3,19 @@ package ovh.pignol.JavaMarketAtHack;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 
 public class MarketAtHackApplication extends Application
 {
+	private double aYPadding = ((double)25.0);
 	private Stage aWindow;
 	private Label aPasswordLabel;
 	private PasswordField aPasswordField;
+	private Button aStartButton;
+	private Button aCloseButton;
 
 	@Override
 	public void start(Stage pWindow) throws Exception
@@ -20,16 +24,22 @@ public class MarketAtHackApplication extends Application
 		this.aWindow.setTitle("Market At Hack Selenium Automaton");
 		this.aPasswordLabel = new Label("Password");
 		this.aPasswordField = new PasswordField();
+		this.aPasswordField.setLayoutY(this.aYPadding * 1);
 		this.aPasswordField.setAccessibleHelp("Password");
+		this.aStartButton = new Button("Start...");
+		this.aStartButton.setLayoutY(this.aYPadding * 2);
+		this.aStartButton.setDefaultButton(true);
+		this.aCloseButton = new Button("Close");
+		this.aCloseButton.setLayoutY(this.aYPadding * 3);
+		
 		Group vRoot = new Group();
 		this.aWindow.setScene(new Scene(vRoot));
-		vRoot.getChildren().addAll(this.aPasswordLabel, this.aPasswordField);
+		vRoot.getChildren().addAll(this.aPasswordLabel, this.aPasswordField, this.aStartButton, this.aCloseButton);
+		this.aWindow.show();
 	}
 	
 	public Stage mWindow()
 	{
 		return this.aWindow;
 	}
-	
-	
 }
